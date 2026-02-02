d624d81aa0a7dad39312c4ac1c452abdb6410e10
Mohammad Anas <mohammadanas8948@gmail.com>
2026-02-03 00:09:15 +0530

    Implement UserService

--- backend/src/main/java/com/productivity/backend/service/UserService.java
+++ backend/src/main/java/com/productivity/backend/service/UserService.java
@@ -1,4 +1,51 @@
 package com.productivity.backend.service;
+import com.productivity.backend.config.SecurityConfig;
+import com.productivity.backend.model.User;
+import com.productivity.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+
+@Service
 public class UserService {
+    @Autowired
+    public UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // Method to register a user
+    public User registerUser(String username, String email, String password){
+
+//        check  if user exist
+
+        if(userRepository.existsByUsername(username)){
+            throw new RuntimeException("Username is already taken");
+        }
+        if(userRepository.existsByEmail(email)){
+            throw new RuntimeException("Email already in use");
+        }
+
+//      create a new user
+
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+
+//      Encrypt the password before saving!
+
+        newUser.setPassword(passwordEncoder.encode(password));
+
+        return userRepository.save(newUser);
+
+
+    }
+    // Method to Find a User by Username
+
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
 }
