2f4cc9a32bc3b697fa6c896d593bf9a8bba43215
Mohammad Anas <mohammadanas8948@gmail.com>
2026-02-10 23:38:45 +0530

    Implement Burnout Calculations

--- backend/src/main/java/com/productivity/backend/service/AnalyticsService.java
+++ backend/src/main/java/com/productivity/backend/service/AnalyticsService.java
@@ -30,6 +30,41 @@ public class AnalyticsService {
         if(logs.isEmpty()){
             return new AnalyticsResponse(); // Returns nulls/defaults
         }
+//        // 2. Calculate Averages (Java Streams API)
+        double avgSleep = logs.stream().mapToDouble(DailyLog::getSleepHours).average().orElse(0.0);
+        double avgWork = logs.stream().mapToDouble(DailyLog::getWorkHours).average().orElse(0.0);
+        double avgStress = logs.stream().mapToDouble(DailyLog::getStressLevel).average().orElse(0.0);
 
+        // 3. Apply Business Rules (The "Drift Detection")
+        AnalyticsResponse response = new AnalyticsResponse();
+        response.setAverageSleepHours(Math.round(avgSleep * 10.0) / 10.0); // Round to 1 decimal
+        response.setAverageWorkHours(Math.round(avgWork * 10.0) / 10.0);
+        response.setAverageStress(Math.round(avgStress * 10.0) / 10.0);
+
+//        RUlE 1: Burnout Risk Calculation
+//        High Risk: Sleep < 6 OR Stress > 4
+        if(avgSleep < 6.0 || avgStress > 4.0){
+            response.setBurnoutRisk("HIGH");
+            response.setRecommendation("Warning: Your biological metrics are critical. Prioritize sleep immediately.");
+        }
+        // Moderate Risk: Sleep < 7 OR Work > 10
+        else if (avgSleep < 7.0 || avgWork > 10.0) {
+            response.setBurnoutRisk("MODERATE");
+            response.setRecommendation("Caution: You are pushing your limits. Consider taking a break.");
+        }
+        else {
+            response.setBurnoutRisk("LOW");
+            response.setRecommendation("Great job! You are maintaining a healthy balance.");
+        }
+//        Productivity Drift (Simple Version)
+//        Check if the most recent log has lower work hours than the average
+        DailyLog lastLog = logs.get(logs.size() - 1);
+        if(lastLog.getWorkHours() < (avgWork - 1.5)){
+            response.setIsProductivityDropping(true); // you drifted significantly
+        }
+        else{
+            response.setIsProductivityDropping(false);
+        }
+        return response;
     }
 }
